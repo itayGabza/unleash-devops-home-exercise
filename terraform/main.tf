a2bcb4fc66f0302b370ffb0a791f796d8fbc88fb
itayg <itayg@voyager-labs.com>
2024-07-13 22:05:59 +0300

    terraform

---
+++ terraform/main.tf
@@ -0,0 +1,82 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "homeAssignment-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Name = "homeAssignment-vpc"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "homeAssignment-cluster"
+  cluster_version = "1.30"
+
+  cluster_endpoint_public_access  = true
+
+#   cluster_addons = {
+#     coredns                = {}
+#     eks-pod-identity-agent = {}
+#     kube-proxy             = {}
+#     vpc-cni                = {}
+#   }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  # EKS Managed Node Group(s)
+#   eks_managed_node_group_defaults = {
+#     instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+#   }
+
+  eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["m5.xlarge"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
+    }
+  }
+
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
+
+#   access_entries = {
+#     # One access entry with a policy associated
+#     example = {
+#       kubernetes_groups = []
+#       principal_arn     = "arn:aws:iam::123456789012:role/something"
+
+#       policy_associations = {
+#         example = {
+#           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+#           access_scope = {
+#             namespaces = ["default"]
+#             type       = "namespace"
+#           }
+#         }
+#       }
+#     }
+#   }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
